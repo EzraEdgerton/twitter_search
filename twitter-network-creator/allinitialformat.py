00a6815f18d4aca7a512346180f91b75619a261c
edgerton <edgerton@grinnell.edu>
2017-02-15 10:07:57 -0600

    moving to git

---
+++ twitter-network-creator/allinitialformat.py
@@ -0,0 +1,135 @@
+import sys
+import json
+import os
+import secondaryformat
+import twitter_folder_change
+
+def typecheck(term_type, term):
+	if term_type == 'hashtag':
+		return '#' + term
+	elif term_type == 'username' or term_type == 'text':
+		return term
+	else:
+		sys.exit('USAGE: use either "hashtag", "handle", or "text" for type of term')
+
+def search(values, searchFor):
+	for k in values:
+		if k['name'] == searchFor:
+			return k
+	return -1
+
+def additional_group(value, additional_terms,term_type):
+	terms_number = len(additional_terms)
+	group = 0
+	finder = ''
+	group_list = []
+	terms_list = []
+	for i in range(0, terms_number):
+		if term_type == 'text' or term_type == 'hashtag':
+			val = value['text'].find(additional_terms[i])
+			if val!= -1:
+				group = group + i
+				finder = value['text'][val: val + 2]
+				group_list.append(i)
+				terms_list.append(additional_terms[i])
+				#print finder
+		if term_type == 'username':
+			if value['user']['screen_name'].find(additional_terms[i]) != -1:
+				group = i
+	print terms_list
+	return [group_list, terms_list]
+
+def create_circle_lists(groups, list_of_terms):
+	ratio = (float(1)/float(len(groups))) * len(list_of_terms)
+	angles = []
+	#print ratio
+	for i in range(0, len(groups)):
+		angles.append([i * ratio, (i + 1) * ratio, groups[i], 1])
+	return angles
+
+
+
+def firstformat(start_day, end_day, search_terms, filename, search_type):
+	search_term = search_terms[0]
+	additional_terms = search_terms
+	day_user_data=[]
+	index = 0
+	for day in range(start_day, end_day):
+		with open('twitter-network-creator/filtered_data/all'+str(day)+filename+'.json') as data_file:
+		#with open('filtered_data/all10BlackLivesMatterAllLivesMatterMichaelBrownFergusonPolice.json') as data_file:	 
+			data = json.load(data_file)
+			for d in data:
+				searchval=search(day_user_data, d['user']['screen_name'])
+				sub_searched = additional_group(d, additional_terms, search_type)
+				groups_in_tweet = sub_searched[0]
+				terms_in_tweet = sub_searched[1]
+				angles =  create_circle_lists(groups_in_tweet, search_terms)
+				if searchval != -1:
+					if 'retweeted_status' in d:
+						searchval['links'].append(d['retweeted_status']['user']['screen_name'])
+					searchval['score'] = searchval['score'] + 1
+					searchval['text'].append(d['text'])
+					for angle in angles:
+						angle[3] = angle[3] + 1
+					searchval['group'] = searchval['group'] + list(set(searchval['group']) - set(groups_in_tweet))#additional_group(d, additional_terms, search_type)
+					searchval['angles'][0] = create_circle_lists(searchval['group'], search_terms)
+					searchval['angles'][1] = searchval['score']
+					searchval['tweets_in_text'] = searchval['tweets_in_text'] + list(set(searchval['tweets_in_text']) - set(terms_in_tweet))
+				else:
+					if 'retweeted_status' not in d:
+						thing={
+							'text' : [d['text']],
+							'name': d['user']['screen_name'],
+							'score': 1,
+							'links': [],
+							'group' : groups_in_tweet,#additional_group(d, additional_terms, search_type),
+							'url' : d['id'],
+							'index' : index,
+							'retweets' : 0,
+							'angles' : [angles, 1],
+							'tweets_in_text': terms_in_tweet
+							}
+					else:
+						thing={
+							'text' : [d['text']],
+							'name': d['user']['screen_name'],
+							'score': 1,
+							'links': [d['retweeted_status']['user']['screen_name']],
+							'group' : groups_in_tweet,#additional_group(d, additional_terms, search_type),
+							'url' : d['id'],
+							'index' : index,
+							'retweets' : 0,
+							'angles' : [angles, 1],
+							'tweets_in_text': terms_in_tweet
+							}
+						addnode=search(day_user_data, d['retweeted_status']['user']['screen_name'])
+						if addnode == -1:
+							index = index + 1
+							newnode={
+								'text' : [d['text']],
+								'name': d['retweeted_status']['user']['screen_name'],
+								'score': 1,
+								'links': [],
+								'group' : groups_in_tweet,#additional_group(d, additional_terms, search_type),
+								'url' : d['retweeted_status']['id'],
+								'index' : index,
+								'retweets' : 0,
+								'angles' : [angles, 1],
+								'tweets_in_text': terms_in_tweet
+								}
+							day_user_data.append(newnode)
+					index = index + 1
+					day_user_data.append(thing)
+		print day
+	filename = 'all' + filename
+	with open('twitter-network-creator/half_formatted_data/half_formatted'+ str(start_day) + '-' + str(end_day) + '-' + filename +'.json', 'w') as outfile:
+		json.dump(day_user_data, outfile)
+	secondaryformat.secondformat(start_day, end_day, search_terms, filename)
+
+firstformat(10, 11, ['BlackLivesMatter', 'AllLivesMatter', 'MichaelBrown', 'FergusonPolice'], 'BlackLivesMatterAllLivesMatterMichaelBrownFergusonPolice', 'hashtag')
+
+	   # for x in day_user_data:
+		#	if len(x['links']) > 1:
+		#		print x['links']
+
+
---
+++ twitter-network-creator/allsearch.py
@@ -0,0 +1,93 @@
+# All this created by Ezra Edgerton June 2016 - February 2017 for the University of Cincinnati. Copyright etc. to me.
+#
+#
+import sys
+import os
+import json
+import allinitialformat
+import secondaryformat
+import twitter_folder_change
+
+
+
+start_day =  int(sys.argv[1])
+end_day = int(sys.argv[2])
+
+#search_term = sys.argv[3]
+
+argument_len = len(sys.argv)
+
+search_type = sys.argv[argument_len - 1]
+
+def search(values, searchFor):
+    for k in values:
+    	if k == searchFor:
+    		return k
+    return -1 
+
+
+search_terms = []
+
+filename = ''
+
+for i in range(3, argument_len - 1):
+	filename = filename + sys.argv[i]
+	
+for i in range(3, argument_len - 1):
+	search_terms.append(allinitialformat.typecheck(search_type, sys.argv[i]))
+
+
+
+print filename
+
+#search_term = allinitialformat.typecheck(search_type,search_term)
+
+
+twitter_folder_change.network_directory_parent()
+
+for day in range(start_day, end_day):
+	tweets=[]
+	if day < 10:
+		daystring='0' + str(day)
+	else:
+		daystring=str(day)
+	#for hour in range(0, 24):
+	for hour in range(6, 18):
+		if hour < 10:
+				hourstring='0'+str(hour)
+		else:
+			hourstring=str(hour)
+		for minute in os.listdir(daystring+'/'+hourstring):
+			if minute.startswith('.'):
+				continue
+			if minute < 10:
+				minutestring='0'+str(minute)
+			else:
+				minutestring=str(minute)
+			print str(day)+'/'+hourstring+'/'+minutestring
+			for line in open(daystring+'/'+hourstring+'/'+minutestring, 'r'):
+				tweet = json.loads(line)
+				for search_term in search_terms:
+					if search_type == 'username':
+						if 'user' not in tweet:
+							continue
+						if tweet['user']['screen_name'].find(search_term) != -1:
+							if tweets.find(tweet) == -1:
+								print tweet["text"]
+								print tweet['user']['screen_name']
+								tweets.append(tweet)
+					else:
+						if "text" not in tweet:
+							continue
+						if tweet["text"].find(search_term) != -1:
+							if search(tweets, tweet) == -1:
+								print tweet["text"]
+								tweets.append(tweet)
+
+
+	with open('twitter-network-creator/filtered_data/all'+str(day)+filename + '.json', 'w') as outfile:
+		json.dump(tweets, outfile, indent=4)
+
+allinitialformat.firstformat(start_day, end_day, search_terms, filename, search_type)
+
+
